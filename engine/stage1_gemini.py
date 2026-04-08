5e55df76e22394c68b3c342a3c8fa2720661d7ac
sebastiankoczen <92438786+sebastiankoczen@users.noreply.github.com>
2026-04-08 15:55:06 +0200

    Update stage1_gemini.py

--- engine/stage1_gemini.py
+++ engine/stage1_gemini.py
@@ -116,7 +116,6 @@ def _extract_grounding_sources(resp):
 def parse_result(text):
     clean = re.sub(r"```[a-z]*", "", text)
     clean = re.sub(r"[\*\`#~]+", "", clean)
-    clean = re.sub(r"<[^>]+>", "", clean)
     clean = clean.strip()
 
     out = {}
